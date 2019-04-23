81226cedd7278c0a7bff95d8c4aa071d0b84ed37
Loic Tiberghien <loic@macbook-air-de-loic-2.home>
2019-04-23 10:27:36 +0200

    Created the rest of the scenarios

--- Project1/WS_NoIL.py
+++ Project1/WS_NoIL.py
@@ -14,9 +14,9 @@ from sub_modules import Parallel_Net, Analyzer_Net, evaluateClassIdentification,
 '''
 Construction of the basic net with :
 
-    - No weight sharing
+    - Weight sharing
 
-    - Intermediate/auxiliary loss
+    - No intermediate/auxiliary loss
 
 Definition of the corresponding training function, test of the resulting net.    
 
@@ -31,8 +31,7 @@ train_input, train_target, train_classes, test_input, test_target, test_classes 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.parallel_net1 = Parallel_Net()
-        self.parallel_net2 = Parallel_Net()
+        self.parallel_net = Parallel_Net()
         self.analyser_net  = Analyzer_Net()
     
     def forward(self,x):
@@ -41,58 +40,40 @@ class Net(nn.Module):
         x2 = x[:,1,:,:].view(-1,1,14,14)
 
         # No weight sharing (declare 2 distinct instances of Parallel_Net)
-        x1 = self.parallel_net1(x1)
-        x2 = self.parallel_net2(x2)
+        x1 = self.parallel_net(x1)
+        x2 = self.parallel_neta(x2)
 
         # Concatenate back both classification results 
         x = torch.cat((x1.view(-1,10),x2.view(-1,10)),dim=1)
         x = self.analyser_net(x)
 
-        return x , x1, x2
+        return x 
 
 
-def train_network(model, train_input, train_target, train_classes, mini_batch_size):
+def train_network(model, train_input, train_target, mini_batch_size):
     train_target_One_Hot = torch.eye(2)[train_target]
-
     # Specify the loss function
-    criterion_total = nn.MSELoss()
-    criterion_classifier = nn.CrossEntropyLoss()
+    criterion = nn.MSELoss()
 
     # Define the number of epochs to train the network
     epochs = 25
-
-    # Set the learning rate
-    eta = 0.02
     
-    loss_record_total=[]
-    loss_record_classifier=[]
+    # Set the learning rate
+    eta = 0.1
+    loss_record=[]
     for e in range(epochs):
-        sum_loss_total = 0
-        sum_loss_classifier = 0
+        sum_loss = 0
         for b in range(0, train_input.size(0), mini_batch_size):
+            output = model(train_input.narrow(0, b, mini_batch_size)) # dim,start,length
+            loss = criterion(output, train_target_One_Hot.narrow(0, b, mini_batch_size))
+            sum_loss = sum_loss + loss.item()
             model.zero_grad()
-            output, x1, x2 = model(train_input.narrow(0, b, mini_batch_size)) # dim,start,length
-            y = [x1, x2]
-            l =[0, 1]
-            for x,i in zip(y,l):
-                # For each image (since there are 2 channels)
-                loss_classifier = criterion_classifier(x , train_classes[:,i].narrow(0, b, mini_batch_size))
-                sum_loss_classifier += loss_classifier.item()
-                loss_classifier.backward(retain_graph=True)
-
-            loss_total = criterion_total(output, train_target_One_Hot.narrow(0, b, mini_batch_size))
-            sum_loss_total += loss_total.item()
-            loss_total.backward()
-
+            loss.backward()
             for p in model.parameters():
                 p.data.sub_(eta * p.grad.data)
-        loss_record_total.append(sum_loss_total)
-        loss_record_classifier.append(sum_loss_classifier)
-        print('Sum of classifier loss at epoch {}: \t'.format(e),sum_loss_classifier)  
-        print('Sum of total loss at epoch {}: \t'.format(e),sum_loss_total)  
-
-    return model, loss_record_total , loss_record_classifier
-
+        loss_record.append(sum_loss)
+        print('Sum of loss at epoch {}: \t'.format(e),sum_loss)
+    return model, loss_record
 
       
 def evaluateFinalOutput(model, test_input, test_target, mini_batch_size):
@@ -101,7 +82,7 @@ def evaluateFinalOutput(model, test_input, test_target, mini_batch_size):
     with torch.no_grad():
         error = 0
         for b in range(0, test_input.size(0), mini_batch_size):
-            output, _, _ = model(test_input.narrow(0, b, mini_batch_size))
+            output = model(test_input.narrow(0, b, mini_batch_size))
             for i in range(output.size(0)):
                 if torch.argmax(output[i]) == 1:
                     if test_target.narrow(0, b, mini_batch_size)[i].item() < 0.2:
@@ -123,8 +104,8 @@ num_param = sum(p.numel() for p in basicModel.parameters() if p.requires_grad)
 print('Number of trainable parameters:',num_param)  
 
 # Train the network
-basicModel, _, _ = train_network(basicModel,train_input, train_target, train_classes, mini_batch_size)
-print(type(basicModel))
+basicModel, loss_record = train_network(basicModel,train_input, train_target, mini_batch_size)
+
 
 # Evaluate the performance of the model
 res = evaluateFinalOutput(basicModel,test_input,test_target,mini_batch_size)

b6768c27ffbd034b1f360fb3bc9d9f86abfb2694
Lucas Waelti <lucas.waelti@gmail.com>
2019-04-25 21:11:41 +0200

    Added doc to Master.py

--- Project1/Master.py
+++ Project1/Master.py
@@ -1,1 +1,5 @@
-blooop
+'''
+    This file automatically trains and runs all networks and collects data about each configuration. 
+    It then generates some figures illustrating the performance of the different architectures. 
+'''
+

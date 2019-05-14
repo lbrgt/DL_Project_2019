542de386fd0f1d145437ec60c1106dcd978d0845
Lucas Burget <lucas.burget@epfl.ch>
2019-05-14 16:26:01 +0200

    Cleaning and commenting

--- Project2/NeuralNet.py
+++ Project2/NeuralNet.py
@@ -2,56 +2,80 @@ import torch
 import math
 #%%
 
-class SGDOptimizer: 
-    def __init__(self, eta=0.01, momentum= 0.9):
-        self.layer_memory = dict()
-        self.step_size = eta
+class SGDOptimizer:
+    '''
+        Stochastic Gradient Descent Optimizer update the weight and bias of the corresponding layers.
+        The learning rate, momentum and decay rate can be defined. 
+    ''' 
+    def __init__(self, learning_rate=0.01, momentum= 0.9, decay = 0.0):
+
+        self.layer_memory = dict() # Store previous cumulative gradient
+        self.learning_rate = learning_rate
         self.momentum = momentum
+        self.decay = decay
 
-
-    def __call__(self, layer):
+    def __call__(self, layer, epoch):
+        # Concatenate to ease the computation
         g = torch.cat([layer.dl_dw_cumulative, layer.dl_db_cumulative],0)
+
+        # Retrieve previous cumulative gradient
         if layer in self.layer_memory:
             u_t_1 = self.layer_memory[layer]
         else:
             u_t_1 = torch.empty(g.shape).fill_(0)
-        
-        u_t = self.momentum*u_t_1 + self.step_size * g
+
+        # Decreasing learning rate to allow fast and non-oscillating convergence
+        lrate = self.learning_rate * (1 / (1 + self.decay * epoch))
+
+        # Momentum implementation     
+        u_t = self.momentum*u_t_1 + lrate * g
+
         layer.weight = layer.weight - u_t[:-1,:] 
         layer.bias = layer.bias - u_t[-1,:]     
         self.layer_memory[layer] = u_t
 
 class AdamOptimizer:
+    '''
+        Adam Optimizer update the weight and bias of the corresponding layers.
+    ''' 
     def __init__(self, beta_1=0.9, beta_2=0.99, step_size=0.001, epsilon=0.000001):
-        self.layer_memory_m = dict()
-        self.layer_memory_v = dict()
+        self.layer_memory_m = dict()    # Store values from previous step
+        self.layer_memory_v = dict()    # Store values from previous step
         self.beta_1 = beta_1
         self.beta_2 = beta_2
         self.step_size = step_size
         self.epsilon = epsilon
 
     def __call__(self, layer):
+
+        # Concatenate to ease the computation       
         g = torch.cat([layer.dl_dw_cumulative, layer.dl_db_cumulative],0)
+
+        # Retrives values from previous step
         if layer in self.layer_memory_v:
             m_t_1 = self.layer_memory_m[layer]
             v_t_1 = self.layer_memory_v[layer]
         else:
             m_t_1 = torch.empty(g.shape).fill_(0)
             v_t_1 = torch.empty(g.shape).fill_(0)
+
+        # Applies Adam formula
         m_t = self.beta_1 * m_t_1 + (1 - self.beta_1) * g
         v_t = self.beta_2 * v_t_1+ (1 - self.beta_2) * torch.pow(g, 2)
         m_hat = m_t / (1 - self.beta_1)
         v_hat = v_t / (1 - self.beta_2)
-
         w = torch.cat([layer.weight, layer.bias], 0)
-        truc = w - self.step_size * m_hat / (torch.sqrt(v_hat) + self.epsilon)
+        A = w - self.step_size * m_hat / (torch.sqrt(v_hat) + self.epsilon)
 
-        layer.weight = truc[:-1,:]
-        layer.bias = truc[-1,:].view(1,-1)
+        layer.weight = A[:-1,:]
+        layer.bias = A[-1,:].view(1,-1)
         self.layer_memory_m[layer] = m_t
         self.layer_memory_v[layer] = v_t
         
-class DLModule:  
+class DLModule: 
+    '''
+        Main module which embeds the structure of the neural network and provide function to deal with it. Can proccess batch of sample.
+    '''  
 
     def __init__(self, *layer, optimizer = SGDOptimizer()):
         self.layer = []
@@ -59,8 +83,8 @@ class DLModule:
         if layer :
             self.sequential(layer)
 
-
-    def __str__(self): # NOTE - add a print overload to easily display the stored architecture 
+    # Print overload to easily display the stored architecture
+    def __str__(self):  
         value = 'Model architecture:\n'
         for layer in self.layer:
             value += str(layer) 
@@ -70,8 +94,11 @@ class DLModule:
             else:
                 value += '\n'
         return value
-
-    def displayParameters(self): # NOTE - display the network's parameters values 
+ 
+    def displayParameters(self): 
+        '''
+            Display the network's parameters values
+        '''
         value = "Model's parameters:\n\n"
         for layer in self.layer:
             if type(layer).__name__ is 'Linear':
@@ -85,43 +112,61 @@ class DLModule:
         return value 
 
     def sequential(self, layer):
+        '''
+            Create the structure of the network by storing the different layers. They have to implement bacward and forward functions.
+        '''
         for item in layer: 
             if hasattr(item, 'forward') and hasattr(item, 'backward'):
                 self.layer.append(item)  
             else: 
                 raise Exception("The specified argument should implement forward() and backward() methods") 
 
-    def __call__(self, input):  
+    def __call__(self, input):
+        '''
+            Following Pytorch notation, compute one forward pass trough the network.
+        '''  
         for node in self.layer:
             input = node.forward(input)
         return input
     
     def backward(self, loss:list): 
-        output = loss[1] # dloss 
+        '''
+            Compute one backward pass trough the network, updating the cumulative gradient of the different layers. 
+        '''
+        output = loss[1]  
         for node in list(reversed(self.layer)):
             output = node.backward(output)   
 
-    def update(self, *epoch):
+    def update(self, epoch):
+        '''
+            Update the parameters of the different layers, usinig the Optimizer defined. It use the cumulative gradient value stored on the different layer. 
+        '''
         for layer in self.layer:
             try:
-                self.optimizer(layer) 
-                
+                self.optimizer(layer, epoch) 
             except Exception as e: 
                 pass
     
     def zero_grad(self):
+        '''
+            Reset the cumulative gradient values to zero.
+        '''
         for layer in self.layer:
             if type(layer).__name__ is 'Linear':
                 layer.zero_grad()
 
 class LossMSE:
+    '''
+        Implement the Mean Squared Error Loss. Compute loss and the derivative of it to backpropagate.  
+    '''
     def __init__(self):
         self.loss = None
 
     def __call__(self, output, target): 
         '''
-            Both output and target must satisfy .view(-1,"size of sample")
-            Returns a list of 2 tensors [loss dloss] 
+            Both output and target must satisfy .view(-1,"size of sample").
+            If classification, the "target" tensor has to be one hot encoded.
+            Returns a list of 2 tensors [loss dloss]. 
         '''
         self.loss = self.eval(output, target)
         self.dloss = self.evald(output, target)
@@ -133,7 +178,10 @@ class LossMSE:
     def evald(self, output, target):
         return 2*(output-target)    
 
-class Tanh:    
+class Tanh: 
+    '''
+        Implement tanh activation function. Implement forward() and backward().
+    '''   
     def eval(self, x):
         return x.tanh()
 
@@ -141,6 +189,9 @@ class Tanh:
         return 4 * (x.exp() + x.mul(-1).exp()).pow(-2)
 
     def forward(self, input):
+        '''
+            Store the input tensor to be used during the backward pass. 
+        '''
         self.input = input    
         return self.eval(input)
 
@@ -148,6 +199,9 @@ class Tanh:
         return dl_dx * self.evald(self.input)   
 
 class Sigmoid: 
+    '''
+        Implement sigmoid activation function. Implement forward() and backward().
+    '''       
     def eval(self, x):
         return torch.sigmoid(x)
 
@@ -155,6 +209,9 @@ class Sigmoid:
         return torch.sigmoid(x)*(1 - torch.sigmoid(x))
 
     def forward(self, input):
+        '''
+            Store the input tensor to be used during the backward pass. 
+        '''
         self.input = input    
         return self.eval(input)
 
@@ -162,6 +219,9 @@ class Sigmoid:
         return dl_dx * self.evald(self.input)  
 
 class Relu:
+    '''
+        Implement Relu activation function. Implement forward() and backward().
+    '''  
     def eval(self, x):
         return x.relu()
     
@@ -191,31 +251,23 @@ class Linear:
             grad_bias [1 x output_dim]
         '''
         
+        # Better initialisation to reduce the vanishing of the gradient
         std = math.sqrt(2.0 / (input_dim + output_dim))
-        
         self.weight =           torch.empty((input_dim, output_dim)).normal_(0, std)
         self.bias =             torch.empty(1, output_dim).normal_(0, std)
+
         self.dl_dw_cumulative = torch.empty((input_dim, output_dim)).fill_(0)
         self.dl_db_cumulative = torch.empty(1, output_dim).fill_(0)
         
 
-    
     def forward(self, x):
         self.input_previous = x
         return x @ self.weight + self.bias
 
     def backward(self, dl_ds):
-        '''
-            dl_dx received
-            compute dl_ds = dl_dx .*dF(self.s)
-            grad_weight dl_dw_cumulative += dl_ds*self.input ( input from forward pass)
-            grad_bias dl_db_cumulative += dl_ds
-            generate dl_dx_(l-1) for next layer : weight*dl_ds
-        '''
         dl_dx = dl_ds @ self.weight.transpose(0,1)
-        self.dl_dw_cumulative += self.input_previous.transpose(0,1) @ dl_ds
-        self.dl_db_cumulative += dl_ds.sum(0)
-      
+        self.dl_dw_cumulative += self.input_previous.transpose(0,1) @ dl_ds # Sums up over the whole batch
+        self.dl_db_cumulative += dl_ds.sum(0)                               # Sums up over the whole batch
         return dl_dx
 
     def update_param(self, eta):
@@ -227,12 +279,18 @@ class Linear:
         self.dl_db_cumulative.fill_(0)
 
 class CrossEntropyLoss():
+    '''
+        Implement the Cross entropy Loss. 
+        Compute loss and the derivative of it to backpropagate.  
+    '''
     def __init__(self):
         self.loss = None
+        self.dloss = None
 
     def __call__(self, output, target): 
         '''
             Both output and target must satisfy .view(-1,"size of sample")
+            The "target" tensor has to contain the label [0, nbr_class-1].
             Returns a list of 2 tensors [loss dloss] 
         '''
         self.loss = self.eval(output, target)
@@ -240,7 +298,7 @@ class CrossEntropyLoss():
         return [self.loss, self.dloss]
 
     def softmax(self, T):
-        exps = torch.exp(T) # - torch.max(T, 1)[0].view(-1,1))
+        exps = torch.exp(T - torch.max(T, 1)[0].view(-1,1)) # Avoid log extrema
         return exps/torch.sum(exps,1).view(-1,1)
     
     def eval(self, output, target):
@@ -253,5 +311,5 @@ class CrossEntropyLoss():
         proba = self.softmax(output)
         n_sample = target.shape[0]
         proba[range(n_sample),target.type(torch.LongTensor)] -= 1
-        return proba /n_sample
+        return proba
 
--- Project2/test.py
+++ Project2/test.py
@@ -1,6 +1,7 @@
 #%%
 import sys
 import os
+import time
 
 sys.path.append(os.getcwd()+'/Project2/')
 
@@ -25,30 +26,36 @@ model = DLModule(
     Relu(),
     Linear(25,2),
     Sigmoid(),
-    optimizer=SGDOptimizer(eta=0.001, momentum= 0.5)
+    optimizer=SGDOptimizer(learning_rate=0.01, momentum= 0.5, decay=0.1) # OPTION: AdamOptimizer()
 )
+
 # Display its architecture
 print(model)
+
 # Display its parmeters
 model.displayParameters()
 #%%
 
 # Visualize the model's initial behaviour 
 showBehaviour = lambda model,input: dg.plotDataset(input,model(input).argmax(1).view(-1)) 
-#showBehaviour(model,test_input)
 
 #%%
-# Define a loss - NOTE: only has to be instantiated once now
-criterion = LossMSE()
+# Define a loss -Has to be instantiated only once
+criterion = LossMSE() # OPTION: CrossEntropyLoss()
 
+# Convert the target generated with label into a one hot encoded vector for the MSE Loss
 train_target
 train_target_OH = torch.empty((train_target.shape[0],int(train_target.max().item()+1))).fill_(0.)
 train_target_OH[range(train_target.shape[0]), train_target.type(torch.LongTensor)] = 1
 
-# %%
+input("Press enter to continue")
+
 # Train the network
-epochs = 100
+# The syntax is as close as possible as pytorch
+epochs = 200
 mini_batch_size = 200
+
+start = time.time()
 for e in range(epochs):
     sum_loss = 0
     # We do this with mini-batches
@@ -56,15 +63,27 @@ for e in range(epochs):
 
         output = model(train_input.narrow(0, b, mini_batch_size))
         loss = criterion(output, train_target_OH.narrow(0, b, mini_batch_size))
-        sum_loss = sum_loss + loss[0].item() # NOTE - loss a bit dirty but okay? 
+        sum_loss = sum_loss + loss[0].item()
         model.zero_grad()
         model.backward(loss)
-        model.update() 
+        model.update(e) 
          
     print(e, '-', sum_loss)
 
-print(model(test_input).argmax(1).view(-1, 1))
-showBehaviour(model,test_input)    
+# Compute the learning time
+stop = time.time()
+duration = stop-start
+print("This model, with {} epoch and a batch size of {}, was trained in {:.2f}s".format(epochs, mini_batch_size, duration))
 
-print(torch.sum(model(test_input).argmax(1).int()==test_target.int()).item())
-#%%
+# Compute the error for the train set
+train_output = model(train_input)
+nbr_error = torch.sum(train_output.argmax(1).int()!=train_target.int()).item()
+print('After training, the ratio of missclassified point for the train set is {} % '.format(100*nbr_error/test_target.shape[0]))
+
+# Compute the error for the test set
+test_output = model(test_input)
+nbr_error = torch.sum(test_output.argmax(1).int()!=test_target.int()).item()
+print('After training, the ratio of missclassified point for the test set is {} % '.format(100*nbr_error/test_target.shape[0]))
+
+# Plot the output of the model for the test set
+showBehaviour(model,test_input)    
